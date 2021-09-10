a9d49aee593fa7f68117bdb1a61bb095aa685e43
Kejovaara <andre@kejovaara.se>
2021-09-10 11:17:22 +0200

    Created methods for entering combat, ending combat

--- src/main/java/helloworld/combat/Combat.java
+++ src/main/java/helloworld/combat/Combat.java
@@ -1,4 +1,32 @@
 package helloworld.combat;
 
-public class Combat {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combat implements ICombat {
+
+    private List<ICombat> players = new ArrayList<ICombat>();
+
+    ICombat player;
+    ICombat opponent;
+
+    public Combat(ICombat player, ICombat opponent){
+        this.player = player;
+        this.opponent = opponent;
+    }
+
+    public void enterCombat(){
+        players.add(this.player);
+        players.add(this.opponent);
+    }
+
+    public void checkCombat(){
+        for(ICombat combatPlayers : players){
+
+        }
+    }
+
+    public void removeFromCombat(ICombat player, ICombat opponent){
+
+    }
 }
