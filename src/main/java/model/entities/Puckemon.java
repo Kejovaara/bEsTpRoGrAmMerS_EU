395d25c4b9a5ecec6a9ae52403b16dae12093bd5
RasmusAlmryd <rasmus0427@gmail.com>
2021-09-17 11:36:29 +0200

    started working on the foundation of the effect system (Rasmus and Lukas)

--- src/main/java/model/entities/Puckemon.java
+++ src/main/java/model/entities/Puckemon.java
@@ -3,6 +3,21 @@ package model.entities;
 public class Puckemon {
     private int health = 100;
     private String name = "Puckemon";
+    private int attackPowerBuffFactor = 0;
+    private int baseAttackPower;
+    private int baseDefence;
+    private int baseSpeed;
+
+    private int currentAttackPower;
+    private int currentDefence;
+    private int currentSpeed;
+
+    private void changeCurrentAttackPower(){
+        currentAttackPower = (int) (baseAttackPower * (1 + attackPowerBuffFactor * 0.25));
+    }
+
+
+
 
     public int getHealth() {
         return health;
