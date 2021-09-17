367421620ffce5290e1cd245edb7dcd23e441607
RasmusAlmryd <rasmus0427@gmail.com>
2021-09-17 11:35:56 +0200

    started working on the foundation of the effect system (Rasmus and Lukas)

--- src/main/java/model/effects/IEffectContainer.java
+++ src/main/java/model/effects/IEffectContainer.java
@@ -1,4 +1,17 @@
 package model.effects;
 
+import java.util.List;
+
 public interface IEffectContainer {
+
+    public int getPriority();
+    public List<IEffect> getEffects();
+
+    public void hpSteal();
+    public void doDamage();
+    public void healYourselfAmount();
+    public void healYourselfPercentage();
+
+
+
 }
