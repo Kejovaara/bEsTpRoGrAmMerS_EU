ff9b4324bb4a9ff317b859fed7384f1bf42798e6
Luke-png <72192057+Luke-png@users.noreply.github.com>
2021-09-22 22:15:56 +0200

    Added more Puckemon methods. Secured some methods for null and negative ints issues. Implemented formulas for leveling and experience.

--- src/main/java/model/entities/Puckemon.java
+++ src/main/java/model/entities/Puckemon.java
@@ -12,15 +12,15 @@ public class Puckemon {
     private int baseDefence;
     private int baseSpeed;
 
-    private int evolutionLevel;
-    private int evolutionID;
+    private int evolutionLevel = 101;
+    private int evolutionID = 0;
 
     // -------------
 
     /**
      * These are a pokemons stats. They are solely based on the baseStats and current level.
      */
-    private int health;
+    private int maxHealth;
     private int attackPower;
     private int defence;
     private int speed;
@@ -34,6 +34,8 @@ public class Puckemon {
     private int currentSpeed;
 
     private int attackPowerBuffFactor = 0;
+    private int defenceBuffFactor = 0;
+    private int speedBuffFactor = 0;
     private int level;
     private int expPoints;
 
@@ -52,24 +54,61 @@ public class Puckemon {
     private void buildPuckemon(int id){
         // Collects data from Excel depending on the ID
         // ex: this.baseSpeed = "excel.baseSpeed id 1"
+        calculateLevelStats();
+
     }
 
     private void calculateLevelStats(){
-        this.health = (2*baseHealth+level)/100 + level + 10;
+        this.maxHealth = (2*baseHealth+level)/100 + level + 10;
         this.attackPower = (2*baseAttackPower+level)/100+5;
         this.defence = (2*baseDefence+level)/100+5;
         this.speed = (2*baseSpeed+level)/100+5;
     }
 
-    private void checkLevelUp(){
 
+    /**
+     * base exp (68 - 220)ish * level * trainer ( 1,5) = gained exp
+     * expPoints needed to reach level x is x^3
+     */
+    private void gainExp(int experience){
+        if (expPoints < (100^3)){
+            expPoints += experience;
+            while (expPoints > ((level+1)^3)){
+                levelUp();
+            }
+        }
     }
 
-    private void changeCurrentAttackPower(){
+    private void levelUp(){
+        if (level < 100){
+            level++;
+        }
+        if (level >= evolutionLevel) {
+//           This should only happen after battle, not right in the middle
+//            buildPuckemon(evolutionID);
+        }else{
+//            The pokemon keeps the same health percentage when it levels up.
+            int healthPercentage = currentHealth/maxHealth;
+            calculateLevelStats();
+            currentHealth = maxHealth/healthPercentage;
+        }
+    }
+
+    private void alterCurrentStats(){
         if (attackPowerBuffFactor < 0){
             currentAttackPower = (int) (attackPower) * (2 / (2 + (-1) * attackPowerBuffFactor));
         }else{
             currentAttackPower = (int) (attackPower * (1 + attackPowerBuffFactor * 0.25));
+        }
+        if (defenceBuffFactor < 0){
+            currentDefence = (int) (defence) * (2 / (2 + (-1) * defenceBuffFactor));
+        }else{
+            currentDefence = (int) (defence * (1 + defenceBuffFactor * 0.25));
+        }
+        if (speedBuffFactor < 0){
+            currentSpeed = (int) (speed) * (2 / (2 + (-1) * speedBuffFactor));
+        }else{
+            currentSpeed = (int) (speed * (1 + speedBuffFactor * 0.25));
         }
     }
 
