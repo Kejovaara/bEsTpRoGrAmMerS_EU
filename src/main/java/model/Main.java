604d103b629048ea4ce0d76e332dbf789d7fecdd
André Kejovaara <74005387+Kejovaara@users.noreply.github.com>
2021-09-22 14:59:10 +0200

    Added Window class and created a frame together with a start and stop method in Main

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -3,10 +3,40 @@ package model;
 import model.entities.Player;
 import model.entities.Puckemon;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends Canvas implements Runnable {
+
+	public static final int WIDTH = 1080, HEIGHT = WIDTH /(16*9);
+	private Thread thread;
+	private boolean running = false;
+
+	public Main(){
+		new Window(WIDTH, HEIGHT, "Puckemon", this);
+	}
 
 	public static void main(String args[]){
 
 	}
 
+	public synchronized void start(){
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+
+	public synchronized void stop(){
+		try{
+			thread.join();
+			running = false;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		// NEED A RUN METHOD
+	}
 }
