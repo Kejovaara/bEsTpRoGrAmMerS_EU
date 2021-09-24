bb35b6b945217bae39de8c53beb0832c7b30d93e
RasmusAlmryd <rasmus0427@gmail.com>
2021-09-24 12:23:34 +0200

    Created two new classes that implements IEffect that can change puckemon hp based on percentage or amount

---
+++ src/main/java/model/effects/effectTypes/HealAmount.java
@@ -0,0 +1,27 @@
+package model.effects.effectTypes;
+
+import model.effects.IEffect;
+import model.entities.IPuckemon;
+
+public class HealAmount implements IEffect {
+    int amount;
+    Boolean healYourself;
+
+    public HealAmount(int amount){
+        this.amount = amount;
+        healYourself = true;
+    }
+
+    public HealAmount(int amount, Boolean healYourself){
+        this.amount = amount;
+        this.healYourself = healYourself;
+    }
+    @Override
+    public void execute(IPuckemon attackUser, IPuckemon opponent) {
+        if(healYourself){
+            attackUser.setHealth(attackUser.getHealth() + amount);
+        }else{
+            opponent.setHealth(attackUser.getHealth() + amount);
+        }
+    }
+}
