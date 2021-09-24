c280eb8456d6ab520825f480ae5e8317798155da
RasmusAlmryd <rasmus0427@gmail.com>
2021-09-24 10:42:10 +0200

    Attack class done for now, maybe need additional functionality later

--- src/main/java/model/combat/Attack.java
+++ src/main/java/model/combat/Attack.java
@@ -1,13 +1,36 @@
 package model.combat;
 
 import model.PuckemonTypes;
+import model.effects.IEffect;
+import model.effects.IEffectContainer;
+import model.entities.IPuckemon;
 
-public class Attack {
-    private int damage;
-    private PuckemonTypes type;
+import java.util.ArrayList;
+import java.util.List;
 
-    public void execute(){
+public class Attack implements IEffectContainer {
+    private int priority;
+    private List<IEffect> effects;
 
+    public Attack(int priority, List<IEffect> effects){
+        this.priority = priority;
+        this.effects = effects;
     }
 
+    @Override
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public List<IEffect> getEffects() {
+        return effects;
+    }
+
+    @Override
+    public void execute(IPuckemon attackUser, IPuckemon opponent) {
+        for (IEffect effect: effects) {
+            effect.execute(attackUser, opponent);
+        }
+    }
 }
