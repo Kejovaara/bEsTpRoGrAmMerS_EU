360625e7d4aff8e49451d4c57e9e61e96ee7b2d8
Kejovaara <andre@kejovaara.se>
2021-10-01 15:52:54 +0200

    Added buyItem and useItem for Player

--- src/main/java/model/entities/Player.java
+++ src/main/java/model/entities/Player.java
@@ -9,10 +9,12 @@ public class Player implements ITrainer {
     private String name = "Bamse";
     private PuckeBag puckeBag;
     private Inventory inventory;
+    private int coins;
 
-    public Player(ArrayList<Puckemon> puckemons, ArrayList<Item> items){
+    public Player(ArrayList<Puckemon> puckemons, ArrayList<Item> items, int coins){
         this.puckeBag = new PuckeBag(puckemons);
         this.inventory = new Inventory(items);
+        this.coins = coins;
     }
 
     public Puckemon selectPuckemon(){
@@ -33,6 +35,19 @@ public class Player implements ITrainer {
     }
 
     public void addItem(Item item){ inventory.addItem(item);}
+
+    public void useItem(Item item){
+        // item.execute(this);
+    }
+
+    public void buyItem(Item item){
+        if(this.coins >= item.getValue()){
+            this.coins -= item.getValue();
+            inventory.addItem(item);
+        }else{
+            System.out.println("You don't have enough PuckeCoins for this item!");
+        }
+    }
 
     public void addPuckemon(Puckemon puckemon) {
         puckeBag.add(puckemon);
