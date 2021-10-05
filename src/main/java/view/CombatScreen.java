847461e52f8733c72a8c746a141afe81128e0172
André Kejovaara <74005387+Kejovaara@users.noreply.github.com>
2021-10-05 20:25:29 +0200

    Added font, label for testing

--- src/main/java/view/CombatScreen.java
+++ src/main/java/view/CombatScreen.java
@@ -2,8 +2,11 @@ package view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.ScreenUtils;
 import model.Model;
 import model.entities.Puckemon;
@@ -30,6 +33,15 @@ public class CombatScreen implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false);
 
+        //FONT
+        Label.LabelStyle fontStyle = new Label.LabelStyle();
+        BitmapFont font = new BitmapFont(Gdx.files.internal("fonts/pixelfont.ttf"));
+        fontStyle.font = font;
+        fontStyle.fontColor = Color.BLACK;
+
+        Label label = new Label("Test text 123",fontStyle);
+
+
         playerPuck = getTexture(playerPuckemon.getId(), false);
         trainerPuck = getTexture(trainerPuckemon.getId(), true);
         //pucke2 = new Texture(Gdx.files.internal("PuckemonBack/1.png"));
@@ -55,6 +67,7 @@ public class CombatScreen implements Screen {
         game.batch.draw(background, 0, 0, this.camera.viewportWidth, this.camera.viewportHeight);
         game.batch.draw(trainerPuck, 600, 400, 128, 128);
         game.batch.draw(playerPuck, 200, 167, 128, 128);
+
         game.batch.end();
     }
 
