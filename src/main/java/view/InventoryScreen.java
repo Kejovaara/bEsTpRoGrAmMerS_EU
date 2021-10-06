287b287626e1430ba3cb36af3c5bee616781e913
André Kejovaara <74005387+Kejovaara@users.noreply.github.com>
2021-10-06 15:32:48 +0200

    More inventory screen work

--- src/main/java/view/InventoryScreen.java
+++ src/main/java/view/InventoryScreen.java
@@ -24,7 +24,7 @@ public class InventoryScreen implements Screen {
     private BitmapFont inventoryFont;
 
     OrthographicCamera camera;
-    Texture itemList, descriptionBox, background;
+    Texture descriptionBox, background;
 
     public InventoryScreen(final Boot game, Model model){
         this.game = game;
@@ -38,7 +38,7 @@ public class InventoryScreen implements Screen {
 
         shapeRenderer = new ShapeRenderer();
 
-        inventoryFont = new BitmapFont(Gdx.files.internal("pixelfont.fnt"));
+        inventoryFont = new BitmapFont(Gdx.files.internal("fonts/pixelfont.fnt"));
         inventoryFont.getData().setScale(0.75f);
 
         Label.LabelStyle fontStyle = new Label.LabelStyle();
@@ -46,27 +46,42 @@ public class InventoryScreen implements Screen {
         fontStyle.fontColor = Color.BLACK;
 
         //Item list
-        Label itemListLabel = new Label("Item List", fontStyle);
+        Label itemListLabel = new Label("ITEM LIST", fontStyle);
         itemListLabel.setSize(520, 10);
-        itemListLabel.setPosition(50,500);
+        itemListLabel.setPosition(100,500);
         itemListLabel.setWrap(false);
         stage.addActor(itemListLabel);
 
+        //Item test
+        Label itemTestLabel = new Label("Small Healing Potion", fontStyle);
+        itemTestLabel.setSize(520, 10);
+        itemTestLabel.setPosition(100,475);
+        itemTestLabel.setWrap(false);
+        stage.addActor(itemTestLabel);
+
+        //Item quantity
+        Label itemAmountLabel = new Label("Item amount", fontStyle);
+        itemAmountLabel.setSize(520, 10);
+        itemAmountLabel.setPosition(400,475);
+        itemAmountLabel.setWrap(false);
+        stage.addActor(itemAmountLabel);
+
         //Item description
-        Label itemDescriptionLabel = new Label("Item List", fontStyle);
+        Label itemDescriptionLabel = new Label("Small Healing Potion", fontStyle);
         itemDescriptionLabel.setSize(200, 10);
-        itemDescriptionLabel.setPosition(50,500);
+        itemDescriptionLabel.setPosition(600,500);
         itemDescriptionLabel.setWrap(true);
         stage.addActor(itemDescriptionLabel);
 
         //BACK BUTTON
         Label backButtonLabel = new Label("Back to combat", fontStyle);
         backButtonLabel.setSize(75,10);
-        backButtonLabel.setPosition(50,125);
+        backButtonLabel.setPosition(100,50);
         backButtonLabel.setWrap(false);
         stage.addActor(backButtonLabel);
 
         background = new Texture(Gdx.files.internal("inventory_background.png"));
+        descriptionBox = new Texture(Gdx.files.internal("inventory_description_box.png"));
 
 
     }
@@ -77,6 +92,11 @@ public class InventoryScreen implements Screen {
 
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+            game.batch.draw(background, 0, 0, this.camera.viewportWidth,this.camera.viewportHeight);
+            game.batch.draw(descriptionBox, 500,120,407,420);
+        game.batch.end();
 
         stage.act();
         stage.draw();
