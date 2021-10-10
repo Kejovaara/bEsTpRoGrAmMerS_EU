17fd1b2d87d72be1e8808c3be66f2e82001dc6c6
André Kejovaara <74005387+Kejovaara@users.noreply.github.com>
2021-10-10 16:27:56 +0200

    Added RestorePP effect

---
+++ src/main/java/model/effects/effectTypes/RestorePP.java
@@ -0,0 +1,11 @@
+package model.effects.effectTypes;
+
+import model.effects.IEffect;
+import model.entities.IPuckemon;
+
+public class RestorePP implements IEffect {
+    @Override
+    public void execute(IPuckemon attackUser, IPuckemon opponent) {
+
+    }
+}
