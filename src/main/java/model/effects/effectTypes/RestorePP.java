8df09a00d22d14cae2431ff400b809927477c152
André Kejovaara <74005387+Kejovaara@users.noreply.github.com>
2021-10-10 16:46:21 +0200

    Added isCombatItem in Item Class

--- src/main/java/model/effects/effectTypes/RestorePP.java
+++ src/main/java/model/effects/effectTypes/RestorePP.java
@@ -4,8 +4,19 @@ import model.effects.IEffect;
 import model.entities.IPuckemon;
 
 public class RestorePP implements IEffect {
+    int amount;
+    boolean targetYourself;
+
+    public RestorePP(int amount){
+        this.amount = amount;
+        targetYourself = true;
+    }
     @Override
     public void execute(IPuckemon attackUser, IPuckemon opponent) {
-
+        if(targetYourself){
+           //TODO
+        }else{
+            //TODO
+        }
     }
 }
