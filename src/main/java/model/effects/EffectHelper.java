59b8741b632cbc24302ee790557b879a15b1cc97
Luke-png <72192057+Luke-png@users.noreply.github.com>
2021-10-15 10:12:13 +0200

    Small fixes before merge

--- src/main/java/model/effects/EffectHelper.java
+++ src/main/java/model/effects/EffectHelper.java
@@ -7,12 +7,12 @@ import java.util.List;
 import java.util.Random;
 
 public class EffectHelper {
-    private static PTypes[] types= {PTypes.NORMAL, PTypes.FIRE, PTypes.WATER, PTypes.ELECTRIC, PTypes.GRASS,
+    private final static PTypes[] types= {PTypes.NORMAL, PTypes.FIRE, PTypes.WATER, PTypes.ELECTRIC, PTypes.GRASS,
                                     PTypes.ICE, PTypes.FIGHTING, PTypes.POISON, PTypes.GROUND, PTypes.FLYING,
                                     PTypes.PSYCHIC, PTypes.BUG, PTypes.ROCK, PTypes.GHOST, PTypes.DRAGON, PTypes.DARK,
                                     PTypes.STEEL, PTypes.FAIRY};
 
-    private static double[][] typeChart = {
+    private final static double[][] typeChart = {
             //1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18
             {1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.0, 1.0, 1.0, 0.5, 1.0},
             {1, 0.5, 0.5, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 0.5, 1.0, 2.0, 1.0},
@@ -38,9 +38,13 @@ public class EffectHelper {
     private static double getMultplier(PTypes attackType, List<PTypes> defendTypes){
         int attackIndex = getIndex(attackType);
         int defendIndex1 = getIndex(defendTypes.get(0));
-        int defendIndex2 = getIndex(defendTypes.get(1));
-        double multiplier = typeChart[attackIndex][defendIndex1] * typeChart[attackIndex][defendIndex2];
-        return multiplier;
+        int defendIndex2 = getIndex(defendTypes.get(defendTypes.size()-1));
+
+        if (defendIndex1!=defendIndex2){
+            return typeChart[attackIndex][defendIndex1] * typeChart[attackIndex][defendIndex2];
+        }else{
+            return typeChart[attackIndex][defendIndex1];
+        }
     }
 
     //error hantering senare kanske;
